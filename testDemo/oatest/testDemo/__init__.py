449c9f2ee25930622032d036406669ab5ede5881
shengcy0417 <shengcy0417@163.com>
2022-07-21 19:44:38 +0800

    首次提交测试

---
+++ testDemo/oatest/testDemo/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time  : 2022/7/21 19:32
+#@Author: shengchunyue
+#@File  : __init__.py.py
---
+++ testDemo/oatest/testDemo/变量多次使用.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time  : 2022/7/21 19:32
+#@Author: shengchunyue
+#@File  : 变量多次使用.py
+
+name='maliya'
+name='chuliubing'
+print(name)
